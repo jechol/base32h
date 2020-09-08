7c2a14bfc7e054b07b52790100714b44a46eb2f0
Jechol Lee <mr.jechol@gmail.com>
2020-09-08 11:13:21 +0900

    Add test

--- lib/base32h.ex
+++ lib/base32h.ex
@@ -1,18 +1,100 @@
-defmodule Base32h do
-  @moduledoc """
-  Documentation for Base32h.
-  """
+defmodule Base32H do
+  @digit_set %{
+    0 => ["0", "O", "o"],
+    1 => ["1", "l", "i"],
+    2 => ["2"],
+    3 => ["3"],
+    4 => ["4"],
+    5 => ["5", "S", "s"],
+    6 => ["6"],
+    7 => ["7"],
+    8 => ["8"],
+    9 => ["9"],
+    10 => ["A", "a"],
+    11 => ["B", "b"],
+    12 => ["C", "c"],
+    13 => ["D", "d"],
+    14 => ["E", "e"],
+    15 => ["F", "f"],
+    16 => ["G", "g"],
+    17 => ["H", "h"],
+    18 => ["J", "j"],
+    19 => ["K", "k"],
+    20 => ["L", "l"],
+    21 => ["M", "m"],
+    22 => ["N", "n"],
+    23 => ["P", "p"],
+    24 => ["Q", "q"],
+    25 => ["R", "r"],
+    26 => ["T", "t"],
+    27 => ["V", "v", "U", "u"],
+    28 => ["W", "w"],
+    29 => ["X", "x"],
+    30 => ["Y", "y"],
+    31 => ["Z", "z"]
+  }
 
-  @doc """
-  Hello world.
+  @encode_map @digit_set |> Enum.map(fn {n, [s | _]} -> {n, s} end) |> Enum.into(%{})
+  @decode_map @digit_set |> Enum.map(fn {n, [s | _]} -> {s, n} end) |> Enum.into(%{})
 
-  ## Examples
+  @min_encode 0
+  # max integer representible by 5 bytes.
+  @max_encode 1_099_511_627_775
+  @min_decode "0"
+  @max_decode "ZZZZ-ZZZZ"
 
-      iex> Base32h.hello()
-      :world
+  def encode(n, allow_starting_zeros \\ false)
+      when is_integer(n) and n >= @min_encode and n <= @max_encode do
+    bin = <<n::40>>
+    raw_encoded = for <<five_bits::size(5) <- bin>>, do: @encode_map |> Map.fetch!(five_bits)
 
-  """
-  def hello do
-    :world
+    chunks = for <<five_bits::size(5) <- bin>>, do: five_bits
+
+    zeros_processed =
+      if allow_starting_zeros do
+        chunks
+      else
+        chunks |> remove_starting_zeros()
+      end
+
+    encoded = zeros_processed |> Enum.map(&Map.fetch!(@encode_map, &1))
+
+    encoded |> Enum.into("")
+  end
+
+  def encode_bin(<<bin::binary>>) do
+    padded = pad(bin)
+    expanded = for <<n::40 <- padded>>, do: expand_int(n)
+
+    expanded
+    |> List.flatten()
+    |> remove_starting_zeros()
+    |> do_encode()
+  end
+
+  defp do_encode(nums) when is_list(nums) do
+    nums
+    |> Enum.map(&Map.fetch!(@encode_map, &1))
+    |> Enum.into("")
+  end
+
+  defp expand_int(n) when is_integer(n) and n >= @min_encode and n <= @max_encode do
+    expand_bin(<<n::40>>)
+  end
+
+  defp expand_bin(<<bin::binary-size(5)>>) do
+    for <<key::5 <- bin>>, do: key
+  end
+
+  defp pad(<<bin::binary>>) do
+    pad_size = rem(byte_size(bin), 5)
+    <<0::pad_size*8, bin::binary>>
+  end
+
+  defp remove_starting_zeros([last_digit]), do: [last_digit]
+  defp remove_starting_zeros([0 | tail]), do: remove_starting_zeros(tail)
+  defp remove_starting_zeros(non_zero_started), do: non_zero_started
+
+  def decode(str) do
   end
 end
--- test/base32h_test.exs
+++ test/base32h_test.exs
@@ -1,8 +1,44 @@
-defmodule Base32hTest do
+defmodule Base32HTest do
   use ExUnit.Case
-  doctest Base32h
+  doctest Base32H
 
-  test "greets the world" do
-    assert Base32h.hello() == :world
+  describe "encoding" do
+    test "encode/1" do
+      assert Base32H.encode(0) == "0"
+      assert Base32H.encode(1_099_511_627_775) == "ZZZZZZZZ"
+
+      assert_raise(FunctionClauseError, fn ->
+        Base32H.encode(-1)
+      end)
+
+      assert_raise(FunctionClauseError, fn ->
+        Base32H.encode(1_099_511_627_776)
+      end)
+
+      assert Base32H.encode(17_854_910) == "H0WDY"
+    end
+
+    test "encode_bin/1" do
+      assert Base32H.encode_bin(<<227, 169, 72, 131, 141, 245, 213, 150, 217, 217>>) ==
+               "WELLH0WDYPARDNER"
+
+      assert Base32H.encode_bin(<<0, 0, 0, 8, 6, 7, 5, 3, 0, 9>>) == "2060W2G6009"
+    end
+  end
+
+  describe "decoding" do
+    test "decode/1" do
+      assert Base32H.decode("0") == 0
+      assert Base32H.decode("ZZZZZZZZ") == 1_099_511_627_775
+
+      assert Base32H.decode("88pzd") == 8_675_309
+    end
+
+    test "decode_bin/1" do
+      assert Base32H.decode_bin("WELLH0WDYPARDNER") ==
+               <<227, 169, 72, 131, 141, 245, 213, 150, 217, 217>>
+
+      assert Base32H.decode_bin("2060W2G6009") == <<0, 0, 0, 8, 6, 7, 5, 3, 0, 9>>
+    end
   end
 end
