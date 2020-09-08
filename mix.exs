11cc920a280d54e88cf0b855db344705b574d7a6
Jechol Lee <mr.jechol@gmail.com>
2020-09-08 12:16:14 +0900

    Add hex package info

--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-defmodule Base32h.MixProject do
+defmodule Base32H.MixProject do
   use Mix.Project
 
   def project do
@@ -7,7 +7,11 @@ defmodule Base32h.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: "Base32H for Elixir",
+      source_url: "https://github.com/jechol/base32h",
+      docs: docs()
     ]
   end
 
@@ -23,6 +27,26 @@ defmodule Base32h.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/jechol/base32h"},
+      maintainers: ["Jechol Lee(mr.jechol@gmail.com)"]
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "readme",
+      name: "base32h",
+      canonical: "http://hexdocs.pm/base32h",
+      source_url: "https://github.com/jechol/base32h",
+      extras: [
+        "README.md"
+      ]
     ]
   end
 end
