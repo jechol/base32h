8cc7a41c1dfad03ebc7d021caa4fee04383005f4
Jechol Lee <mr.jechol@gmail.com>
2020-09-12 10:49:38 +0900

    Remove niv

--- shell.nix
+++ shell.nix
@@ -1,9 +1,12 @@
 let
-  sources = import ./nix/sources.nix;
-  pkgs = import sources.nixpkgs { };
-  jechol-nur = import sources.jechol-nur { };
-  inherit (pkgs.lib) optional optionals;
-in pkgs.mkShell {
-  buildInputs =
-    [ jechol-nur.beam.all.packages.erlang_23_0.elixirs.elixir_1_10_4 ];
+  nixpkgs = import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/20.03.tar.gz";
+    sha256 = "0182ys095dfx02vl2a20j1hz92dx3mfgz2a6fhn31bqlp1wa8hlq";
+  }) { };
+  jechol = import (fetchTarball {
+    url = "https://github.com/jechol/nur-packages/archive/v1.0.tar.gz";
+    sha256 = "0v5fph0rb8bjibbfzapi8pq46hk5aysrmh6x32nxw4p807ix10l0";
+  }) { };
+in nixpkgs.mkShell {
+  buildInputs = [ jechol.beam.all.packages.erlang_23_0.elixirs.elixir_1_10_4 ];
 }
